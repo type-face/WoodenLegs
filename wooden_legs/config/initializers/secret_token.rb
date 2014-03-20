295f2e9fc3deeb0fc1b400ad25e312716cbcfcff
Rob Ring <rjm.ring@gmail.com>
2014-03-19 22:32:12 -0500

    First commit of new rails project.

---
+++ wooden_legs/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+WoodenLegs::Application.config.secret_key_base = '27bfe7739f2b818f531dbca6cd2153a1e8b9cad786c6dea0951371b1c95e7e35904720e836186f6e618e799bd6211296e65ddc52c80e7cd14f5974775f57ddec'
